ba51f7319bc05999c773f0fb2736f4bc5837c702
Tania Madrigal <tmadrigaljimenez10@gmail.com>
2023-11-03 00:38:38 +0000

    segundo commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-
+import json
 api = Blueprint('api', __name__)
 
 
@@ -20,8 +20,14 @@ def handle_hello():
 @api.route('/sign_up', methods=['POST'])
 def handle_new_user():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    body= json.loads(request.data)
+    new_user= User(
+        email= body["email"],
+        user_name= body["user_name"],
+        password= body["password"],
+        is_admin= False
+    ) 
+    db.session.add(new_user)
+    db.session.commit()
 
-    return jsonify(response_body), 200
+    return jsonify({"msg":"usuario creado con exito"}), 200
