611eab748c624de31c75d87eba86064b1c45c207
Tania Madrigal <tmadrigaljimenez10@gmail.com>
2023-11-08 00:51:15 +0000

    registro

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,26 +3,47 @@ import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-const [email, setEmail]= useState("")
-const [user_name, setUser_name]= useState("")
-const [password, setPassword]= useState("")
-const registro =(e)=> {
-	e.preventDefault()
-	actions.registro(email, user_name, password)
-	setEmail("")
-	setPassword("")
-	setUser_name("")
-}
-	return (
-		<div className="text-center mt-5">
-			<from>
-				<div>
-					<input type="email" />
-				</div>
-				<div><input type="text" /></div>
-				<div><input type="password" /></div>
-			</from>
-		</div>
-	);
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [user_name, setUser_name] = useState("");
+  const [password, setPassword] = useState("");
+  const Registro = (e) => {
+    e.preventDefault();
+    actions.registro(email, user_name, password);
+    setEmail("");
+    setPassword("");
+    setUser_name("");
+  };
+  return (
+    <div className="container">
+		<h1>Registro</h1>
+      <form>
+        <div className="row mb-3">
+          <label className="col-sm-2 col-form-label">Username</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="text" value={user_name} onChange={(e)=>setUser_name(e.target.value)}/>
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input type="email" className="form-control" id="inputEmail3"value={email} onChange={(e)=>setEmail(e.target.value)} />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword3" value={password} onChange={(e)=>setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary" onChange={(e)=>Registro(e.target.value)}>
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -16,37 +16,29 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			registro: async(email,user_name,password) => {
+				try {
+					const response = await fetch("https://3001-taniamj10-authenticatio-sn7lvs7alnd.ws-us106.gitpod.io/api/sign_up",{
+						method: "POST",
+						body: JSON.stringify({
+							email: email,
+							user_name: user_name,
+							password: password
+						}),
+						headers: {
+							"Content-type": "application/json"
+						}
+					})
+					if (response.status==200){
+					const data = await response.json()
+					console.log(data)
+					alert("usuario creado con éxito")
+					// 	localStorage.setItem("token", data.access_token)
+					 }
+				} catch (error){
+					console.log(error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
